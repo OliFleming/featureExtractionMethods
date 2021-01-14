5230b12a00aad3200c048ffb5ab6ede51717656f
Lachlan Smith <55677606+Smith-Lachlan@users.noreply.github.com>
2021-01-14 14:44:36 +1100

    Create testBench.m

---
+++ testBench.m
@@ -0,0 +1,14 @@
+
+
+% Author: Lachlan Smith
+% Work address: 8 Little Queen Street, Chippendale NSW 2008.
+% email: lsmi5655@uni.sydney.edu.au
+% Website: https://www.sydney.edu.au/engineering
+% Janurary 2021; Last revision: 14-1-2021
+
+%------------- BEGIN CODE --------------
+
+
+
+
+%------------- END OF CODE --------------
